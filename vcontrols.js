5a58197d065aaa2c788e6ac4ab03f5713b0b074f
deepak5j <gautamxdeepak@gmail.com>
2017-12-15 18:02:22 +0530

    :+1:

--- vcontrols.js
+++ vcontrols.js
@@ -10,13 +10,13 @@ function reload() {
   var video = document.getElementById("myVideo");
   video.load(); 
 }
-
+/*
 function makeLarge() { 
   var video = document.getElementById("myVideo");
   video.width = 1200;
 }
 
-/*
+
 function makeSmall() { 
   var video = document.getElementById("myVideo");
   video.width = 250; 
