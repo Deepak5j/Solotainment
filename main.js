7f9ace434829a33a8ac35de61051b6372ceb7eb2
deepak5j <gautamxdeepak@gmail.com>
2017-12-25 02:05:04 +0530

    Major updates :+1:

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 function change(argument, argument1, argument2) {
       var vid = document.getElementById("myVideo");
       var cid = document.getElementById("myCaption");
+      var u  = "images/"+ argument2;
+      /*document.getElementById("video_poster").style.backgroundImage="url("+u+")";*/
       vid.poster = "images/"+ argument2;
       isSupp = vid.canPlayType("video/mp4");
       if (isSupp == "") {
